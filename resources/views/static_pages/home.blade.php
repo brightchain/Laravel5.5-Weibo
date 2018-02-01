3680b5c2643e639b33c7b5e1955dd8200dce28e0
cbasil <cbasil@126.com>
2018-02-01 17:39:11 +0800

    Finish static pages

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '首页')
+@section('content')
+  <h1>首页</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StaticController@home');
+Route::get('/help','StaticController@help');
+Route::get('/about','StaticController@about');
